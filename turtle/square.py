b84a89462948ce39ac5a950cd9970736577cd31c
Sergio Enriquez <eqz.nava@gmail.com>
2019-11-21 21:53:34 -0600

    Primer ejercicio

---
+++ turtle/square.py
@@ -0,0 +1,14 @@
+import turtle
+
+window = turtle.Screen()
+jack = turtle.Turtle()
+
+jack.forward(50)
+jack.left(90)
+jack.forward(50)
+jack.left(90)
+jack.forward(50)
+jack.left(90)
+jack.forward(50)
+
+turtle.mainloop()
